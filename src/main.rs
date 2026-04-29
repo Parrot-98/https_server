f95f0334d5a453ae86a97e33c0b4e6c33a48927a
karthikeya <karthikeya777666@gmail.com>
2026-04-28 21:58:56 -0400

    refomated the code and added 404.html page and made it so that the thread sleeps for 5 secs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // start server 
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // start server
 
     //start accepting connections 
     for stream in listener.incoming() {
@@ -18,19 +20,22 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream); // turn the connection into readable text 
-    let http_request: Vec<_> = buf_reader
-        .lines() // reads one line at a time
-        .map(|result| result.unwrap()) // extract strings
-        .take_while(|line| !line.is_empty())// read until a white space as heared in html eand with it
-        .collect();
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap(); // reading the request
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    let (status_line, filename) = if request_line == "GET /sleep HTTP/1.1"
+     {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")}
+    else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"); // print this on to the server form the html 
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"); // writeing the file in the browser 
 
     stream.write_all(response.as_bytes()).unwrap();
 }
